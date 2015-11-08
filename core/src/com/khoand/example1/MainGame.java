0ddd02433a171e9917c00394062ab9578c23bdd8
khnguyendinh <khnguyendinh@gmail.com>
2015-11-08 20:12:41 +0700

    khoa.nd 8.11.2015 sysout demo commit

--- core/src/com/khoand/example1/MainGame.java
+++ core/src/com/khoand/example1/MainGame.java
@@ -14,14 +14,15 @@ public class MainGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		System.out.println("demo commit");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 }
